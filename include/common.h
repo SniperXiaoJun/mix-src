2e5a44a50890ff1cb603d5aee5bcb47940e6a8ce
stoneskill <244540499@qq.com>
2016-12-27 10:55:15 +0800

    add commons

--- include/common.h
+++ include/common.h
@@ -15,6 +15,20 @@
 #define COMMON_API 
 #endif
 
+// redefine func and class and route
+#define __PASTE(x,y) x##y
+
+#ifndef MIX_PREFIX_UPAPI_CLASS_STR
+#define MIX_PREFIX_UPAPI_CLASS_STR "com/wtsecure/safecard/"
+#endif
+
+#ifndef MIX_PREFIX_UPAPI_FUNC
+#define MIX_PREFIX_UPAPI_FUNC Java_com_wtsecure_safecard_SoftToken_
+#endif
+
+#define __MIX_PREFIX_FUNC_PASTE(X,Y) __PASTE(X,Y)
+#define __MIX_PREFIX_STR_PASTE(X,Y) X Y
+
 // debug
 #ifdef _DEBUG
 #define DEBUG(format,...) printf("File: "__FILE__", Line: %05d: "format"\n", __LINE__, ##__VA_ARGS__);
@@ -25,5 +39,6 @@
 #define BUFFER_LEN_1K 1024
 #define SIZE_1K 1024
 #define COUNT_1K 1024
+#define MAX_BUFFER_LEN 1024 * 1024
 
 #endif
