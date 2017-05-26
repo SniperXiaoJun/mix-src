12a457b919cdc51300397e203e2ef55ce4fa4cb1
stoneskill <244540499@qq.com>
2017-05-26 10:18:31 +0800

    NEW QT_UILIB

--- include/qt_uilib.h
+++ include/qt_uilib.h
@@ -1,18 +1,13 @@
 #ifndef QT_UILIB_H
 #define QT_UILIB_H
 
-#include "WTF_Interface.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+	int QT_UILIB_ShowUI(unsigned char *pCertContent, unsigned int uiCertContentLen);
+#ifdef __cplusplus
+}
+#endif
 
-class QT_UILib
-{
-public:
-	QT_UILib();
-	~QT_UILib();
-
-	static int ShowUI(int argc, char * argv[], SK_CERT_CONTENT * pCertContent);
-
-private:
-
-};
 
 #endif // QT_UILIB_H
