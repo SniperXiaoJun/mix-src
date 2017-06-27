7f8fd3f43d5fcd0e82d224ad5fe78bc3d1716374
stoneskill <244540499@qq.com>
2017-06-27 16:10:29 +0800

    fix find certchains

--- src/WTF_Interface/WTF_Interface.c
+++ src/WTF_Interface/WTF_Interface.c
@@ -2947,6 +2947,118 @@ err:
 	return ulRet;
 }
 
+
+
+int GetExtAuthorityIdentifier(PCCERT_CONTEXT pCertContext,unsigned char *lpscProperty, unsigned int* pulLen)
+{
+	int ulRes = 0;
+	DWORD ulDataLen = 512;
+	int ulPropertyLen = 512;
+	BYTE btData[512] = { 0 };
+	CHAR csProperty[512] = { 0 };
+	PCERT_AUTHORITY_KEY_ID2_INFO pAuthorityKeyID2 = NULL;
+	PCERT_EXTENSION pCertExt = NULL;
+
+	if (!pCertContext)
+	{
+		return -1;
+	}
+	if (!pulLen)
+	{
+		return -1;
+	}
+
+	pCertExt = CertFindExtension(szOID_AUTHORITY_KEY_IDENTIFIER2, pCertContext->pCertInfo->cExtension, pCertContext->pCertInfo->rgExtension);
+	if (!pCertExt)
+	{
+		return -1;
+	}
+
+	pAuthorityKeyID2 = (PCERT_AUTHORITY_KEY_ID2_INFO)btData;
+	if (CryptDecodeObject(X509_ASN_ENCODING | PKCS_7_ASN_ENCODING | CRYPT_ASN_ENCODING, szOID_AUTHORITY_KEY_IDENTIFIER2,
+		pCertExt->Value.pbData, pCertExt->Value.cbData,
+		CRYPT_DECODE_NOCOPY_FLAG, pAuthorityKeyID2, &ulDataLen))
+	{
+
+	}
+	else
+	{
+		return GetLastError();
+	}
+
+	if (!lpscProperty)
+	{
+		*pulLen = pAuthorityKeyID2->KeyId.cbData;
+	}
+	else if (*pulLen < pAuthorityKeyID2->KeyId.cbData)
+	{
+		return -1;
+	}
+	else
+	{
+		*pulLen = pAuthorityKeyID2->KeyId.cbData;
+		memcpy(lpscProperty, pAuthorityKeyID2->KeyId.pbData, *pulLen);
+	}
+
+	return 0;
+}
+
+int GetExtSubjectIdentifier(PCCERT_CONTEXT pCertContext,
+	unsigned char * lpscProperty,
+	unsigned int* pulLen)
+{
+	int ulRes = 0;
+	DWORD ulDataLen = 512;
+	int ulPropertyLen = 512;
+	BYTE btData[512] = { 0 };
+	CHAR csProperty[512] = { 0 };
+	PCERT_EXTENSION pCertExt = NULL;
+	PCRYPT_DATA_BLOB pDataBlob=NULL;
+
+	if (!pCertContext)
+	{
+		return -1;
+	}
+	if (!pulLen)
+	{
+		return -1;
+	}
+
+	pCertExt = CertFindExtension(szOID_SUBJECT_KEY_IDENTIFIER, pCertContext->pCertInfo->cExtension, pCertContext->pCertInfo->rgExtension);
+	if (!pCertExt)
+	{
+		return -1;
+	}
+
+	pDataBlob = (PCRYPT_DATA_BLOB)btData;
+	if (CryptDecodeObject(X509_ASN_ENCODING | PKCS_7_ASN_ENCODING | CRYPT_ASN_ENCODING, szOID_SUBJECT_KEY_IDENTIFIER,
+		pCertExt->Value.pbData, pCertExt->Value.cbData,
+		CRYPT_DECODE_NOCOPY_FLAG, pDataBlob, &ulDataLen))
+	{
+
+	}
+	else
+	{
+		return GetLastError();
+	}
+
+	if (!lpscProperty)
+	{
+		*pulLen = pDataBlob->cbData;
+	}
+	else if (*pulLen < pDataBlob->cbData)
+	{
+		return -1;
+	}
+	else
+	{
+		*pulLen = pDataBlob->cbData;
+		memcpy(lpscProperty, pDataBlob->pbData, *pulLen);
+	}
+
+	return 0;
+}
+
 unsigned int __stdcall WTF_VerifyCert(unsigned int ulFlag,unsigned int ulAlgType, BYTE* pbCert, unsigned int ulCertLen)
 {
 	unsigned int ulRet = 0;
@@ -3002,7 +3114,24 @@ unsigned int __stdcall WTF_VerifyCert(unsigned int ulFlag,unsigned int ulAlgType
 				}
 
 				// 查找颁发者证书
-				certContext_OUT = CertFindCertificateInStore(hCertStore,X509_ASN_ENCODING,0,CERT_FIND_ISSUER_OF,certContext_IN,NULL);
+				// certContext_OUT = CertFindCertificateInStore(hCertStore,X509_ASN_ENCODING,0,CERT_FIND_ISSUER_OF,certContext_IN,NULL);
+				{
+					CERT_ID id;
+
+					unsigned char data_value_keyid[1024] = {0};
+					unsigned int data_len_keyid = 1024;
+
+					id.dwIdChoice = CERT_ID_KEY_IDENTIFIER;
+
+
+					GetExtAuthorityIdentifier(certContext_IN,data_value_keyid, &data_len_keyid);
+
+					id.KeyId.pbData = data_value_keyid;
+					id.KeyId.cbData = data_len_keyid;
+
+					certContext_OUT = CertFindCertificateInStore(hCertStore,X509_ASN_ENCODING,0,CERT_FIND_CERT_ID,&id,NULL);
+				}
+				
 
 				if (NULL == certContext_OUT)
 				{
@@ -3031,7 +3160,22 @@ unsigned int __stdcall WTF_VerifyCert(unsigned int ulFlag,unsigned int ulAlgType
 					}
 
 					// 查找颁发者证书
-					certContext_OUT = CertFindCertificateInStore(hCertStore,X509_ASN_ENCODING,0,CERT_FIND_ISSUER_OF,certContext_IN,NULL);
+					// certContext_OUT = CertFindCertificateInStore(hCertStore,X509_ASN_ENCODING,0,CERT_FIND_ISSUER_OF,certContext_IN,NULL);
+					{
+						CERT_ID id;
+
+						unsigned char data_value_keyid[1024] = {0};
+						unsigned int data_len_keyid = 1024;
+
+						id.dwIdChoice = CERT_ID_KEY_IDENTIFIER;
+
+						GetExtAuthorityIdentifier(certContext_IN,data_value_keyid, &data_len_keyid);
+
+						id.KeyId.pbData = data_value_keyid;
+						id.KeyId.cbData = data_len_keyid;
+
+						certContext_OUT = CertFindCertificateInStore(hCertStore,X509_ASN_ENCODING,0,CERT_FIND_CERT_ID,&id,NULL);
+					}
 				}
 
 				if (NULL != certContext_OUT)
